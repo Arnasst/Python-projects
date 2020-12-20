6af9dc5512cdb4cf998022508dfe5ae7a82f730e
Arnasst <59619111+Arnasst@users.noreply.github.com>
2020-12-20 20:20:38 +0200

    Added smartAI

--- Tic-Tac-Toe/player.py
+++ Tic-Tac-Toe/player.py
@@ -34,3 +34,47 @@ class HumanPlayer(Player):
                 print('Invalid square. Try again.')
 
             return val
+class SmartComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())
+        else:
+            # get the square with max utility (minimax algorithm)
+            square = self.minimax(game, self.letter)['position']
+        return square
+    
+    def minimax(self, state, player):
+        max_player = self.letter
+        other_player = '0' if player == 'X' else 'X'
+
+        if state.current_winner == other_player:
+            return{'position' : None, 'score' : 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (state.num_empty_squares() + 1)}
+        elif not state.empty_squares(): #while no one won and there are empty square
+            return{'position': None, 'score': 0}
+        
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}
+        else:
+            best = {'position': None, 'score': math.inf}
+        
+        for possible_move in state.available_moves():
+            # make a move and simulate a game after it with recursion
+            state.make_move(possible_move, player)
+            sim_score = self.minimax(state, other_player)
+            # undo the move
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            sim_score['position'] = possible_move # to stop infinite recursion
+            # update dictionary if it is better 
+            if player == max_player: # replace best
+                if sim_score['score'] > best['score']:
+                    best = sim_score
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+        return best
+
+
